ccad65952a4328c5be056c01fc2869618b5dcdb4
amsesk <amsesk@umich.edu>
2023-06-16 11:15:26 -0400

    planck updates

--- keyboards/planck/keymaps/amsesk/keymap.c
+++ keyboards/planck/keymaps/amsesk/keymap.c
@@ -71,12 +71,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
-    KC_LCTL, KC_LCTL, KC_LALT, MO(_NUM),TD(LWRTD),KC_SPC,KC_SPC,TD(RSETD),KC_TAB, KC_DOWN, PATHBACK, LGUI(KC_D)
+    KC_LCTL, KC_LCTL, KC_LALT, KC_TAB,  TD(LWRTD),KC_SPC,KC_SPC,TD(RSETD),KC_RSFT, KC_NO, KC_NO, KC_RCTL
 ),
 
 [_COLEMAK] = LAYOUT_planck_grid(
     _______,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN,_______,
-    _______,  KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,   _______,
+    _______,  KC_A,    KC_R,    KC_S,    MT(MOD_LSFT, KC_T),    KC_D,    KC_H,    MT(MOD_LSFT, KC_N),    KC_E,    KC_I,    KC_O,   _______,
     _______, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 ),
--- users/amsesk/amsesk.h
+++ users/amsesk/amsesk.h
@@ -44,8 +44,8 @@ typedef struct {
 enum {
   SINGLE_TAP = 1,
   SINGLE_HOLD = 2,
-  DOUBLE_TAP = 3//,
-  //DOUBLE_HOLD = 4,
+  DOUBLE_TAP = 3,
+  DOUBLE_HOLD = 4//,
   //DOUBLE_SINGLE_TAP = 5, //send two single taps
   //TRIPLE_TAP = 6,
   //TRIPLE_HOLD = 7
